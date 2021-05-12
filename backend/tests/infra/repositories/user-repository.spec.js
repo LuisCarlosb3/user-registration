f81c2780b92d31e20d5c8d3a2c7c293960e88f3c
Luis Carlos <lu.ca.oliveira41@gmail.com>
2021-05-12 19:57:44 -0400

    test: ensure UserRepository register a new user and address

--- backend/tests/infra/repositories/user-repository.spec.js
+++ backend/tests/infra/repositories/user-repository.spec.js
@@ -8,6 +8,23 @@ const makeUser = () =>{
     cpf:'any_cpf'
   }
 }
+const makeAddress = () =>{
+  return {
+    cep:'any_cep',
+    street:'any_street',
+    complement:'any_complement',
+    district:'any_district',
+    city:'any_city',
+    state:'any_state',
+    number:'any_number'
+  }
+}
+const insertPayload = async()=>{
+  const newUser = await knex('users').insert(makeUser()).returning('id')
+  const address = makeAddress()
+  address.user_id = newUser[0]
+  await knex('user-address').insert(address)
+}
 describe("User Repository", ()=>{
   beforeAll(async () => {
     await knex.migrate.latest()
@@ -16,16 +33,33 @@ describe("User Repository", ()=>{
     await knex.migrate.down()
   })
   beforeEach(async () => {
+    await knex('user-address').del()
     await knex('users').del()
   })
   const makeSut = () => {
     return new UserRepository()
   }
   test('Ensure UserRepository loadUserByEmail with a existent email',async()=>{
-    const sut = makeSut()
-    const data = makeUser()
-    await knex('users').insert(data)
+    const sut = makeSut()   
+    await insertPayload()
     const res = await sut.loadByEmail('any@email')
     expect(res).toBeTruthy()
+  })
+  test('Ensure UserRepository insert a new user on database',async()=>{
+    const sut = makeSut()   
+    const userData = makeUser()
+    const address = makeAddress()
+    await sut.register({...userData, ...address})
+    const user = await knex('users').where({email: 'any@email'}).select()
+    expect(user[0].email).toBe('any@email')
+  })
+  test('Ensure UserRepository calls registerUserAddress on insert new user',async()=>{
+    const sut = makeSut()   
+    const userData = makeUser()
+    const address = makeAddress()
+    await sut.register({...userData, ...address})
+    const user = await knex('users').where({email: 'any@email'}).select()
+    const userAddress = await knex('user-address').where({user_id: user[0].id}).select()
+    expect(userAddress[0]).toBeTruthy()
   })
 })
