f17d182bab3369681f7b092c7acdf71d3381846e
Luis Carlos <lu.ca.oliveira41@gmail.com>
2021-05-12 20:40:44 -0400

    feat: ensure user registration valition returns null on validation succeeds

---
+++ backend/src/validation/user-registration-validation.js
@@ -0,0 +1,32 @@
+import * as yup from 'yup'
+export class UserRegistrationValidation {
+  async validate(userInfo){
+    const userRegistrationSchema = yup.object().shape({
+      name: yup.string().required('Nome é obrigatório'),
+      password: yup.string().required('Senha é obrigatório'),
+      confirm_password: yup.string().oneOf([yup.ref('password'), null], 'Senhas devem ser iguais'),
+      email: yup.string().email().required('E-mail é obrigatório'),
+      cpf: yup.string().required('CPF é obrigatório'),
+      cep: yup.string().required('CEP é obrigatório'),
+      street: yup.string().required('Rua é obrigatório'),
+      complement: yup.string(),
+      district: yup.string().required('Bairro é obrigatório'),
+      city: yup.string().required('Cidade é obrigatório'),
+      state: yup.string().required('Estado é obrigatório'),
+      number: yup.string()
+    })
+    const isValid = await userRegistrationSchema.isValid(userInfo)
+    if(isValid){
+      return null
+    }else{
+      return await userRegistrationSchema.validate(userInfo).catch(err =>{
+        if(err){
+          return err.errors
+        }
+        return null
+      })
+    }
+  }
+}
+
+
